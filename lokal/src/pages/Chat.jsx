855c4155b9d4b3b9dea501801a1f1c15237b5de5
catli-unbounce <catherine.li@unbounce.com>
2024-01-21 13:53:14 -0800

    it works on my computer ¯\_(ツ)_/¯

--- lokal/src/pages/Chat.jsx
+++ lokal/src/pages/Chat.jsx
@@ -1,44 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from "../Navbar"
-import messageService from '../services/messages'
-import '../../chat.css'
+import React, { useEffect, useState } from "react";
+import Navbar from "../Navbar";
+import messageService from "../services/messages";
+import "../../chat.css";
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
 
-    const [messages, setMessages] = useState([])
+  const getMessages = () => {
+    messageService.getAll().then((response) => {
+      console.log("response", response);
+      setMessages(response.data);
+    });
+  };
 
-    const getMessages = async () => {
-        const returnedMessages = await messageService.getAll()
-        console.log(returnedMessages, 'returned')
-        setMessages(returnedMessages)
-    }
+  useEffect(() => {
+    getMessages();
+  }, []);
 
-    useEffect(() => {
-        getMessages()
-    }, [])
-    
-    return (
-        <>
-            <div>
-                <h3 className="name">Ranger Lee</h3>
-                <h4 className='active'>Active 3 mins ago</h4>
-                <div className='msg-container'>
-                    <div className='time time-1'>{messages[0].time}</div>
-                    <div className='first-msg'>
-                        {messages[0].content.slice(7)}
-                    </div>
-                    <div className='time'>{messages[1].time}</div>
-                    <div className='second-msg'>
-                        {messages[1].content.slice(7)}
-                    </div>
-                    <div className='message-bar'>
-                        <input className='message-input'type="text" placeholder='   Message...' />
-                    </div>
-                </div>
+  return (
+    <>
+      {messages.length > 0 && (
+        <div>
+          <h3 className="name">Ranger Lee</h3>
+          <h4 className="active">Active 3 mins ago</h4>
+          <div className="msg-container">
+            <div className="time time-1">{messages[0].id}</div>
+            <div className="first-msg">{messages[0].content.slice(7)}</div>
+            <div className="time">{messages[1].time}</div>
+            <div className="second-msg">{messages[1].content.slice(7)}</div>
+            <div className="message-bar">
+              <input
+                className="message-input"
+                type="text"
+                placeholder="   Message..."
+              />
             </div>
-            <Navbar />
-        </>
-    )
-}
+          </div>
+        </div>
+      )}
+      <Navbar />
+    </>
+  );
+};
 
 export default Chat;
--- lokal/src/services/messages.js
+++ lokal/src/services/messages.js
@@ -1,14 +1,13 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/messages'
+import axios from "axios";
+const baseUrl = "http://localhost:3001/messages";
 
-const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
-}
+const getAll = async () => {
+  return await axios.get(baseUrl);
+};
 
 const create = async (newObject) => {
-    const response = await axios.post(baseUrl, newObject)
-    return response.data
-}
+  const response = await axios.post(baseUrl, newObject);
+  return response.data;
+};
 
-export default { getAll, create }
+export default { getAll, create };
